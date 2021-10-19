9ac2c57a61723e6ebed0d6bf3e87022621a5ef31
Math2121 <matheusdepaula527@gmail.com>
2021-10-19 12:50:43 -0300

    primeiro dia

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,92 @@
+/**
+ * Regra de negócio de AUtenticação da aplicação
+ * Recupera o token de acesso do github
+ * Recuperar infos do github
+ * Verifica se o user está registrado
+ * -- SIM = gera um token
+ * -- NAO = cria o user no BD e gera um token
+ * Retorna as infos do user após a autenticação
+ *
+ *
+ */
+import axios from "axios";
+import prismaClient from "../prisma";
+import {sign} from 'jsonwebtoken'
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    //repassa o code  que foi repassado pelo github
+    const { data: accessToken } = await axios.post<IAccessTokenResponse>(
+      url,
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    // Recupera as informações do  user
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessToken.access_token}`,
+        },
+      }
+    );
+
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+    return { token, user };
+  }
+}
+
+export { AuthenticateUserService };
